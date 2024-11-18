fd165f55d2f3b86a54feff7a50ba5830dad9143c
zulikqmal <ikqmalzul@gamil.com>
2024-11-18 16:32:26 +0800

    First Assignment

--- Car.java
+++ Car.java
@@ -2,7 +2,7 @@ public class Car {
     private String name, model;
     private double price;
 
-    public PrivateVariables(String name, String model, double price) {
+    public Car(String name, String model, double price) {
         this.name = name;
         this.model = model;
         this.price = price;    
@@ -12,11 +12,13 @@ public class Car {
         String nameShow = "Name ";
         String modelShow = "Model ";
 
-        System.out.println(nameShow + name)
-        System.out.println(modelShow + model)
-        System.out.println("Price " + price)
-
-
+        System.out.println(nameShow + name);
+        System.out.println(modelShow + model);
+        System.out.println("Price " + price);
     }
 
+    public static void main(String[] args) {
+        Car cr = new Car ("Honda", "Civic", 200000.00);
+        cr.displayDetails();
+    }
 }
